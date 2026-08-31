bcb93a1abf6aad3d2c486ee124f640913c15052f
Devran Aktı <windssson@hotmail.com>
2026-09-01 02:56:47 +0300

    feat(rpc): merge get_homepage_dashboard and purge unused/overloaded functions

--- lib/features/home/data/home_dashboard_provider.dart
+++ lib/features/home/data/home_dashboard_provider.dart
@@ -4,7 +4,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 final homeDashboardProvider = FutureProvider<HomeDashboardModel>((ref) async {
   final supabase = Supabase.instance.client;
-  final response = await supabase.rpc('get_homepage_dashboard_v2');
+  final response = await supabase.rpc('get_homepage_dashboard');
 
   return HomeDashboardModel.fromJson(
     response is Map<String, dynamic>
