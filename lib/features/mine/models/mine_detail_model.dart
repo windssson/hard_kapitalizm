2aea1949253923e96d76c0e3c77b308db409e7a2
Devran Aktı <windssson@hotmail.com>
2026-09-11 01:09:24 +0300

    fix(mine): decouple configured state from product metadata

--- lib/features/mine/models/mine_detail_model.dart
+++ lib/features/mine/models/mine_detail_model.dart
@@ -172,7 +172,7 @@ class MineDetailModel {
   });
 
   List<ProductionSlotContractModel> get configuredSlots => productionSlots
-      .where((slot) => slot.isConfigured && slot.product != null)
+      .where((slot) => slot.isConfigured)
       .toList(growable: false);
 
   List<ProductionSlotContractModel> get activeConfiguredSlots => configuredSlots
